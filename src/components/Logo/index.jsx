a63000fa5841be5f688836f2ff523c53460eda39
toan215 <toantruongcong2005bee@gmail.com>
2025-10-04 16:43:18 +0700

    [CToan] Add siderbar UI layout

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Flex } from "antd";
+import Logo from "../../assets/icon.svg";
+
+const index = () => {
+  return (
+    <React.Fragment>
+      <Flex gap={10} align="center">
+        <img src={Logo} className="size-10" />
+        <p className="text-4xl font-bold ">M&M</p>
+        <div className="relative size-0.5">
+          <div className="bg-amber-600 w-2 h-2 rounded-3xl relative bottom-0 right-0" />
+        </div>
+      </Flex>
+    </React.Fragment>
+  );
+};
+
+export default index;
---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,143 @@
+import React from "react";
+import Logo from "../Logo";
+import { Container } from "react-bootstrap";
+import { ConfigProvider, Menu } from "antd";
+import { Icon } from "@iconify/react";
+
+const items = [
+  {
+    key: "1",
+    icon: <Icon icon="ion:restaurant-outline" width="24" height="24" />,
+    label: <a href="#">Trang Chủ</a>,
+  },
+  {
+    key: "2",
+    icon: (
+      <Icon
+        icon="material-symbols:workspace-premium-outline"
+        width="24"
+        height="24"
+      />
+    ),
+    label: <p href="#">Premium</p>,
+    children: [
+      {
+        key: "2-1",
+        icon: <Icon icon="mdi:robot-outline" width="24" height="24" />,
+        label: <a href="#">AI Tư Vấn M&M</a>,
+      },
+      {
+        key: "2-2",
+        icon: <Icon icon="mdi:camera-outline" width="24" height="24" />,
+        label: <a href="#">Phân tích Dinh Dưỡng Bằng Ảnh</a>,
+      },
+
+      {
+        key: "2-3",
+        icon: <Icon icon="mdi:food-outline" width="24" height="24" />,
+        label: <a href="#">Tạo Thực Đơn Premium</a>,
+      },
+      {
+        key: "2-4",
+        icon: <Icon icon="mdi:chart-line" width="24" height="24" />,
+        label: <a href="#">Theo Dõi Tiến Độ</a>,
+      },
+      {
+        key: "2-5",
+        icon: <Icon icon="mdi:crown-outline" width="24" height="24" />,
+        label: <a href="#"> Top Thực Đơn Xem Nhiều Nhất</a>,
+      },
+    ],
+  },
+  {
+    key: "3",
+    icon: <Icon icon="mdi:trophy-outline" width="24" height="24" />,
+    label: <a href="#">Thử Thách</a>,
+  },
+  {
+    key: "4",
+    icon: <Icon icon="mdi:bell-outline" width="24" height="24" />,
+    label: <a href="#">Thông Báo</a>,
+  },
+  {
+    key: "5",
+    icon: <Icon icon="ic:outline-settings" width="24" height="24" />,
+    label: <a href="#">Thiết Lập</a>,
+  },
+  {
+    key: "6",
+    icon: <Icon icon="mdi:folder-outline" width="24" height="24" />,
+    label: <p href="#">Kho Món Ngon Của Bạn</p>,
+    children: [
+      {
+        key: "6-1",
+        icon: (
+          <Icon
+            icon="material-symbols:menu-book-2-outline"
+            width="24"
+            height="24"
+          />
+        ),
+        label: <a href="#">Tất Cả</a>,
+      },
+      {
+        key: "6-2",
+        icon: <Icon icon="mdi:bookmark-outline" width="24" height="24" />,
+        label: <a href="#">Đã Lưu</a>,
+      },
+      {
+        key: "6-3",
+        icon: <Icon icon="mdi:account-outline" width="24" height="24" />,
+        label: <a href="#">Món Của Tôi</a>,
+      },
+      {
+        key: "6-4",
+        icon: <Icon icon="et:global" width="24" height="24" />,
+        label: <a href="#">Đã Chia Sẻ</a>,
+      },
+      {
+        key: "6-5",
+        icon: <Icon icon="mdi:file-document-outline" width="24" height="24" />,
+        label: <a href="#">Món Nháp</a>,
+      },
+    ],
+  },
+];
+
+const index = () => {
+  return (
+    <React.Fragment>
+      <ConfigProvider
+        theme={{
+          components: {
+            Menu: {
+              itemColor: "#A098AE",
+              itemHoverColor: "#fff",
+              itemHoverBg: "#F8B602",
+              itemSelectedBg: "#F8B602",
+              itemSelectedColor: "#fff",
+              subMenuItemBg: "#fff",
+              subMenuItemSelectedColor: " #F8B602",
+            },
+          },
+          token: {
+            fontSize: 16,
+          },
+        }}
+      >
+        <Container className="p-5 flex flex-col items-center gap-5">
+          <Logo />
+          <Menu
+            defaultSelectedKeys={"1"}
+            mode="inline"
+            items={items}
+            defaultOpenKeys={["6"]}
+            className="font-sans font-semibold"
+          />
+        </Container>
+      </ConfigProvider>
+    </React.Fragment>
+  );
+};
+
+export default index;
